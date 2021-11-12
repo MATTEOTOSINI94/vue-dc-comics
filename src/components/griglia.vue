772f1c92021af9ca4b9b5537896a78a5f8e80500
MATTEOTOSINI94 <matteotosini300@icloud.com>
2021-11-12 17:29:42 +0100

    overlay card

--- src/components/griglia.vue
+++ src/components/griglia.vue
@@ -1,10 +1,13 @@
 <template>
-   <div class="ciao">
+   <div class="container-card">
                <div class="card">
                    <img :src= img alt="logo">
                    <h4>{{title}}</h4>
-                   <p>{{subtitle}}</p>
+                   
+
+                <div class="overlay"><p>{{subtitle}}+{{prezzo}}</p></div>
                </div>
+
           </div>
 </template>
 
@@ -17,17 +20,41 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    .ciao{
+    .container-card{
         display: flex;
         flex-wrap: wrap;
         width: 100%;
+       
 
         .card{
            width: calc(100% / 6);
+             position: relative;
+            
 
             img{
-                width: 100%
+                width: 100%;
+              
             };
+
+            .overlay{
+                content: "";
+                position: absolute;
+                bottom: 0;
+                left: 0;
+                right: 0;
+                background-color: #f2f0f8;
+               
+                width: 0%;
+                height: 100%;
+                transition: 2s ;
+                
+            }
+
+
         }
+    }
+
+    .card:hover .overlay{
+        width: 100%;
     }
 </style>
